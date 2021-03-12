c71a46e56a9b71ad4bd18ecce595977a9267e668
Ivan <erneman@rambler.ru>
2021-03-12 22:40:59 +0200

    add comments to the task

--- 3-0-spring-mvc/3-0-1-hello-spring-mvc/src/main/java/com/bobocode/mvc/controller/NoteController.java
+++ 3-0-spring-mvc/3-0-1-hello-spring-mvc/src/main/java/com/bobocode/mvc/controller/NoteController.java
@@ -1,12 +1,30 @@
 package com.bobocode.mvc.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-
-@RequestMapping("/notes")
+/**
+ * To make the controller work, you have to add some annotations:
+ * - @Controller - to let spring know that you want make a controller;
+ * - @RequestMapping("/notes") - to indicate the basic endpoint for all request methods;
+ * - @RequiredArgsConstructor (for using storage without explicit autowiring);
+ */
 public class NoteController {
 
-//    TODO Create method to get all notes from storage and pass it to the view
+//    TODO define the storage variable Notes
 
-//    TODO Create method to handle the post request and save new note to the storage
+//    TODO
+//     Create method to get all notes from storage and pass it to the view.
+//     Use Model class for it.
+
+//    TODO
+//     Create method to handle the post request and save new note to the storage
+//     Use @ModelAttribute annotation to mark the method parameter.
+
+
+
+/**
+ * All methods should return String type;
+ * The String should be the name of html page you want to return.
+ * You can find those pages in templates folder.
+ * Thymeleafe use this folder by default to find the web pages;
+ */
 
 }
