a09e16a797eebf597b42025b697862ac32265164
Ivan <erneman@rambler.ru>
2021-03-13 16:01:05 +0200

    Add comments to RestController task

--- 3-0-spring-mvc/3-0-1-hello-spring-mvc/src/main/java/com/bobocode/mvc/api/NoteRestController.java
+++ 3-0-spring-mvc/3-0-1-hello-spring-mvc/src/main/java/com/bobocode/mvc/api/NoteRestController.java
@@ -1,12 +1,23 @@
 package com.bobocode.mvc.api;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-
-@RequestMapping("/api/notes")
+/**
+ * To make the controller work, you have to add some annotations:
+ * - @RestController - to let spring know that you want make a REST controller;
+ * - @RequestMapping("/api/notes") - to indicate the basic endpoint for all request methods;
+ * - @RequiredArgsConstructor (for using storage without explicit autowiring);
+ */
 public class NoteRestController {
 
-//    TODO Create method to get all notes from storage and pass it to the view
+//    TODO define the storage variable Notes
 
-//    TODO Create method to handle the post request and save new note to the storage
+//    TODO
+//     Create method to get list of notes from storage.
 
+//    TODO
+//     Create method to handle the post request and save new note to the storage.
+//     Dont forget about @RequestBody annotation =)
+
+/**
+ * This time all method should return types of concrete data, or ResponseEntity;
+ */
 }
