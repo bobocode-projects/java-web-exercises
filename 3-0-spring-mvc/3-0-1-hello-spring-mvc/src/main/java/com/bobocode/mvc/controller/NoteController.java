ec9c48225b12f1079899cf1f61ee4e8737fa3757
Ivan <erneman@rambler.ru>
2021-03-16 14:01:06 +0200

    change NoteController, change NoteControllerTests

--- 3-0-spring-mvc/3-0-1-hello-spring-mvc/src/main/java/com/bobocode/mvc/controller/NoteController.java
+++ 3-0-spring-mvc/3-0-1-hello-spring-mvc/src/main/java/com/bobocode/mvc/controller/NoteController.java
@@ -2,14 +2,14 @@ package com.bobocode.mvc.controller;
 
 import com.bobocode.mvc.model.Note;
 import com.bobocode.mvc.storage.Notes;
-import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -19,15 +19,23 @@ public class NoteController {
     private final Notes notes;
 
     @GetMapping
-    public String getNotes(Model model) {
-        model.addAttribute("notes", notes.getAll());
-        model.addAttribute("newNote", new Note());
+    public String getNotes() {
         return "notes";
     }
 
     @PostMapping
-    public String addNote(@ModelAttribute Note note) {
+    public String addNote(Note note) {
         notes.add(note);
         return "redirect:/notes";
+    }
+
+    @ModelAttribute("note")
+    private Note getNoteAttr() {
+        return new Note();
+    }
+
+    @ModelAttribute("notes")
+    private List<Note> getNotesAttr() {
+        return notes.getAll();
     }
 }
