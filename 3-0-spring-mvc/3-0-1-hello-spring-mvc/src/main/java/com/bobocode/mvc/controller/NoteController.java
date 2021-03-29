87ac5a3cefae8261195a5d71377a376eb28eff78
Taras Boychuk <tarass.boychuk@gmail.com>
2021-03-29 14:00:11 +0300

    GP-66 update impl

--- 3-0-spring-mvc/3-0-1-hello-spring-mvc/src/main/java/com/bobocode/mvc/controller/NoteController.java
+++ 3-0-spring-mvc/3-0-1-hello-spring-mvc/src/main/java/com/bobocode/mvc/controller/NoteController.java
@@ -4,32 +4,21 @@ import com.bobocode.mvc.model.Note;
 import com.bobocode.mvc.storage.Notes;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/notes")
 public class NoteController {
-
     private final Notes notes;
 
-    @ModelAttribute
-    private List<Note> noteListAttr() {
-        return notes.getAll();
-    }
-
-    @ModelAttribute
-    private Note noteAttr() {
-        return new Note();
-    }
-
     @GetMapping
-    public String getNotes() {
+    public String getNotes(Model model) {
+        model.addAttribute("noteList", notes.getAll());
         return "notes";
     }
 
