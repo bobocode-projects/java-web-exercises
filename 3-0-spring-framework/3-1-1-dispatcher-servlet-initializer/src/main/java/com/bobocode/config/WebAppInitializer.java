75acdf3fee8320d4ae191706728e2c10539d832c
Serhii <mathodcoast@gmail.com>
2021-04-06 17:54:41 +0300

    GP-82 completed solution

--- 3-0-spring-framework/3-1-1-dispatcher-servlet-initializer/src/main/java/com/bobocode/config/WebAppInitializer.java
+++ 3-0-spring-framework/3-1-1-dispatcher-servlet-initializer/src/main/java/com/bobocode/config/WebAppInitializer.java
@@ -9,16 +9,16 @@ import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatche
 public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
     @Override
     protected Class<?>[] getRootConfigClasses() {
-        throw new ExerciseNotCompletedException(); //todo: use {@link RootConfig} as root application config class
+        return new Class[]{RootConfig.class};
     }
 
     @Override
     protected Class<?>[] getServletConfigClasses() {
-        throw new ExerciseNotCompletedException(); //todo: use {@link WebConfig} as ServletConfig class
+        return new Class[]{WebConfig.class};
     }
 
     @Override
     protected String[] getServletMappings() {
-        throw new ExerciseNotCompletedException(); //todo: provide default servlet mapping ("/")
+        return new String[]{"/"};
     }
 }
